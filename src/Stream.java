d885894d9c63b857065968984b7517548d716b14
User Konstantin <Konstantin19111996.Agaltsov@yandex.ru>
2024-05-08 19:59:28 +0300

    Третий коммит

--- src/Stream.java
+++ src/Stream.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Stream {
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Optional;
+
+
+public class Stream {
+    public static void main(String[] args) {
+
+        try {
+
+           Optional<Double> max = Optional.of(new BufferedReader(new FileReader("data.txt")).lines()
+                   .mapToDouble(Stream::parseLineToLength)
+                   .max().orElse(0.0));
+            System.out.println(max.orElseGet(() -> 0.0));
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    private static double parseLineToLength(String line){
+        String[] points = line.split("data.txt");
+        double x1 = Double.parseDouble(points[2]);
+        double y1 = Double.parseDouble(points[4]);
+        double x2 = Double.parseDouble(points[7]);
+        double y2 = Double.parseDouble(points[9]);
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
 }
